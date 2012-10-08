66334be29b54cf92cc4314a2c233c6ae80c4e37f
Dijkstra <me@dijkstra.ru>
2012-10-08 13:14:43 +0600

    Trying to build normalizer

--- src/main/scala/ru/dijkstra/FSMDriver.scala
+++ src/main/scala/ru/dijkstra/FSMDriver.scala
@@ -1,5 +1,8 @@
 package ru.dijkstra
 import ru.dijkstra.ast._
+import collection.{Set, mutable}
+import collection.mutable.ListBuffer
+import collection.immutable.TreeMap
 
 /**
  * FSM interpreter
@@ -36,6 +39,10 @@ class FSMDriver(driven : FSM) {
   def unloadFSM : StringBuilder = {
     var sb = new StringBuilder()
    states map { st => {
+     st.allow match {
+       case true => sb.append("accept ")
+       case false => sb.append("decline ")
+     }
      sb.append(st.name).append(": ").append("\n")
      st.tableRow map {
         a => sb.append("   '").append(a.symbol).append("' ").append(a.newState).append("\n")
@@ -44,5 +51,54 @@ class FSMDriver(driven : FSM) {
    }
    }
    sb
+  }
+  def inputAlphabet =
+    ((states flatMap { _.tableRow map { _.symbol } } toSet) toList) sortWith { _.compareTo(_) < 0 }
+
+
+  def normalForm {
+    val il = states partition(_.allow)
+    val groups = new mutable.HashMap[State, Int]()
+    def out(sym: String, state: State) = (state tableRow) find( _.symbol == sym ) match {
+      case Some(x) => groups get(state) getOrElse(-1)
+      case None => -1
+    }
+    def buildGroups = {
+      val maxIndex = groups maxBy( _._2 ) _2
+      var result = new ListBuffer[List[State]]()
+      for (i <- 0 until maxIndex) {              // = groups clone()
+        val groups2 : mutable.HashMap[State, Int]   = groups clone()
+        val a: mutable.HashMap[State, Int] = groups2 filter { _._2 == i }
+        ////
+        val b: Set[State] = a.map { _._1 } toSeq
+        val c = b toList
+        result.append(c)
+      }
+      result toList
+    }
+    (il _1) map { state => groups += (state -> 0) }
+    (il _2) map { state => groups += (state -> 1) }
+    var newIndex = 2
+    var changesMade = false
+    do {
+     changesMade = false
+     inputAlphabet map {
+       sym => {
+          buildGroups map {
+            group => {
+              val firstout = out(sym, group head)
+              if ((group forall(out(sym, _) == firstout)) == false) {
+                val part = group partition(out(sym, _) == firstout)
+                (part _1) map { state => groups += (state -> firstout) }
+                (part _1) map { state => groups += (state -> newIndex) }
+                newIndex += 1
+                changesMade = true
+              }
+            }
+          }
+        }
+      }
+    } while (changesMade)
+
   }
 }
